a3e0bfea81b160333b0fdfe111c223bc5f630c28
Hicham740 <nh_eddhane@esi.dz>
2026-01-08 14:04:16 +0100

    testing the bramch workflow 3 .

--- src/prompts/auditor_prompt.py
+++ src/prompts/auditor_prompt.py
@@ -35,6 +35,8 @@ def generate_audit_prompt(messy_code_string: str) -> str:
     and returns the full instruction string for the AI.
     """
 
+    
+
     # This acts as a template filler. It looks for {code_content}
     # in the big string and replaces it with the variable passed in.
     formatted_prompt = AUDITOR_SYSTEM_PROMPT.format(code_content=messy_code_string)
